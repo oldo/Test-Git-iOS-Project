cda31a4eb3ecbaa86dacf54c838c2b4e0d5d38fb
oldo.nicho <oldo.nicho@gmail.com>
2014-05-03 23:50:19 +1000

    added test class

---
+++ ONTest.h
@@ -0,0 +1,13 @@
+//
+//  ONTest.h
+//  Git Test
+//
+//  Created by oldo.nicho on 3/05/2014.
+//  Copyright (c) 2014 oldo.nicho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ONTest : NSObject
+
+@end
